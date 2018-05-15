bbf31d86e319ff724586313c17a2461a76b322ef
Eslam Ali <eali@elkrem.io>
2018-05-15 13:55:12 +0200

    Reorganized Config.h file

--- src/Config.h
+++ src/Config.h
@@ -19,10 +19,10 @@
 
 
 /********* Koyn Library Definitions *********/
+
+/* BitcoinAddress class definitions */
 #define P2PKH_ADDRESS	0x00
 #define P2SH_ADDRESS	0x01
-
-#define MAIN_CLIENT	0
 
 #define ADDRESS_NOT_TRACKED 0x00
 #define ADDRESS_NO_FUNDS	0x01
@@ -30,11 +30,6 @@
 #define ADDRESS_NO_PRIVATE_KEY	0x03
 #define ADDRESS_INVALID	0x04
 #define INVALID_AMOUNT	0x05
-
-#define ASN1_BMPSTRING	0x30
-#define ASN1_INTEGER	0x02
-
-#define GENESIS_BLOCK_HASH_TESTNET	"000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
 
 #define MAIN_NET_VERSION 0x00
 #define TEST_NET_VERSION 0x6F
@@ -46,27 +41,18 @@
 #define KEY_PUBLIC 				0x05
 #define KEY_SCRIPT_HASH			0x06
 
-#define SIZE_OF_RIPE_HASH 0x14
+#define MAX_ADDRESSES_TRACKED_REACHED  0x01
+#define MAIN_NET_NOT_SUPPORTED		   0xFF
 
-#define LONGEST_CHAIN_AT_FORK 6
-
-#define ERROR			0
-#define HEADER_VALID	1
-#define SAME_HEADER		2
-#define FORKED			3
-#define FORK_VALID		4
-#define INVALID			5
-
-#define OP_0  0x00
-#define OP_2  0x52
-#define OP_PUSHDATA1 0x4C
-#define OP_DUP	0x76
-#define OP_HASH160 0xA9
-#define OP_EQUALVERIFY	0x88
-#define OP_CHECKSIG	0xAC
-#define OP_EQUAL 	0x87
-
+/* Koyn class definitions */
+#define MAIN_CLIENT	0
+#define GENESIS_BLOCK_HASH_TESTNET	"000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
 #define VERSION_STRING    "2.7.11"
+#define DEFAULT_MAIN_NET_PORT	50001
+
+#define BLOCK_NUM_SUB					0
+#define BLOCK_HEAD_SUB					1
+#define ADDRESS_SUB						2
 
 #define VERSION_BIT						0
 #define ADRRESS_HISTORY_BIT				1
@@ -84,11 +70,38 @@
 #define BROADCAST_TRANSACTION_BIT		13
 #define MERKLE_PROOF					14
 
-#define BLOCK_NUM_SUB					0
-#define BLOCK_HEAD_SUB					1
-#define ADDRESS_SUB						2
+/* BitcoinTransaction class definitions */
+#define ASN1_BMPSTRING	0x30
+#define ASN1_INTEGER	0x02
+#define SIZE_OF_RIPE_HASH 0x14
 
-#define DEFAULT_MAIN_NET_PORT	50001
+#define OP_0  0x00
+#define OP_2  0x52
+#define OP_PUSHDATA1 0x4C
+#define OP_DUP	0x76
+#define OP_HASH160 0xA9
+#define OP_EQUALVERIFY	0x88
+#define OP_CHECKSIG	0xAC
+#define OP_EQUAL 	0x87
 
-#define MAX_ADDRESSES_TRACKED_REACHED  0x01
-#define MAIN_NET_NOT_SUPPORTED		   0xFF
+/* BitcoinFork class definitions */
+#define LONGEST_CHAIN_AT_FORK 6
+
+/* BitcoinHeader class definitions */
+#define ERROR			0
+#define HEADER_VALID	1
+#define SAME_HEADER		2
+#define FORKED			3
+#define FORK_VALID		4
+#define INVALID			5
+
+
+
+
+
+
+
+
+
+
+
