12a231a4815687a545c4cd41eb7540076dd9adef
Islam Mustafa <islam@elkrem.io>
2018-06-19 11:59:50 +0200

    Refactored examples and edited comments

--- examples/BareMinimum/BareMinimum.ino
+++ examples/BareMinimum/BareMinimum.ino
@@ -1,9 +1,32 @@
+/*
+
+Koyn Library Bare Minimum Example
+
+This example shows the bare minimum functions required by the library.
+
+It can be used as a boilerplate for projects.
+
+Note: You need to connect to your WiFi network at the beginning of
+the setup() function before calling Koyn.begin()
+
+*/
+
+/* Include Koyn library. */
 #include "Koyn.h"
 
-void setup() {
-  Koyn.begin(false);
+void setup()
+{
+  /*********************************************************/
+  /*                        REQUIRED                       */
+  /* Connect to your WiFi using ESPWiFi library functions. */
+  /*********************************************************/
+
+  /* Initialize the library and the microSD card. */
+  Koyn.begin();
 }
 
-void loop() {
+void loop()
+{
+  /* Performs the library routines (syncing, validation, tracking, ..etc). */
   Koyn.run();
 }
--- examples/ReceivingBitcoins/ReceivingBitcoins.ino
+++ examples/ReceivingBitcoins/ReceivingBitcoins.ino
@@ -1,69 +1,79 @@
-/* Include ESPWiFi library. */
-#include <ESP8266WiFi.h>
+/*
+
+Receiving Bitcoins Example
+
+By using this example, you can turn on a light bulb relay connected
+to pin 13 when 0.01 Bitcoins is sent to a specific address.
+
+Note: You need to connect to your WiFi network at the beginning of
+the setup() function before calling Koyn.begin()
+
+*/
+
 /* Include Koyn library. */
 #include "Koyn.h"
 
-/* Assign a light bulb to pin13. */
-#define LIGHT_BULB 13
-/* Renting value of light bulb. */
+/* Assign a light bulb to pin 13. */
+#define LIGHT_BULB_RELAY 13
+/* Required payment value for the light bulb in satoshis. (0.01 BTC) */
 #define PAYMENT_VALUE 1000000
-/* Create an address giving it the encoded key address ex."mqBPVCaTaJDVGruNTkTD3CKTVMYqmVow6R" */
-BitcoinAddress myAddress("ENCODED_KEY_ADDRESS",ADDRESS_ENCODED);
-/* Create friend address. */
-BitcoinAddress myFriendAddress("ENCODED_KEY_ADDRESS",ADDRESS_ENCODED);
+/* Create a Bitcoin address with the encoded address ex."mqBPVCaTaJDVGruNTkTD3CKTVMYqmVow6R" */
+BitcoinAddress myAddress("ENCODED_KEY_ADDRESS", ADDRESS_ENCODED);
 
-void setup() {
-  /* Make light bulb pin output. */
-  pinMode(LIGHT_BULB, OUTPUT);
+void setup()
+{
+  /*********************************************************/
+  /*                        REQUIRED                       */
   /* Connect to your WiFi using ESPWiFi library functions. */
-  // CONNECT TO WIFI
-  /* Start Koyn library. */
+  /*********************************************************/
+
+  /* Initialize the library and the microSD card. */
   Koyn.begin();
-  /* Subscribe to a new incomming transaction. */ 
+  /* Subscribe to new incomming transactions with a callback. */
   Koyn.onNewTransaction(&paymentCallback);
+  /* Set light bulb relay pin mode to output. */
+  pinMode(LIGHT_BULB_RELAY, OUTPUT);
 }
 
-void loop() {
-  /* Check first if the board is synced with the network. */
-  if(Koyn.isSynced()){
-    /* Track address. */
-    Koyn.trackAddress(&myAddress);   
+void loop()
+{
+  /* Check if the board is synced with the network. */
+  if (Koyn.isSynced())
+  {
+    /* Track my address to receive transactions callbacks. */
+    Koyn.trackAddress(&myAddress);
   }
-  /* Run the library and check for changes. */
+  /* Performs the library routines (syncing, validation, tracking, ..etc). */
   Koyn.run();
 }
 
-/* Callback function to take a physical action when a transaction is received. */
-void paymentCallback(BitcoinTransaction tx){
-  /* Loop over Inputs and check my friend's address. */
-  for(int i=0;i<tx.getInputsCount();i++)
+/* This callback function will be called when a BTC transaction is received. */
+void paymentCallback(BitcoinTransaction tx)
+{
+  unsigned long totalAmountSentToMe = 0;
+  /* Loop on all transaction outputs to sum up all received bitcoins. */
+  for (int i = 0; i < tx.getOutputsCount(); i++)
   {
-    /* Creating an empty address object. */
-    BitcoinAddress from;
-    /* Retreiving address from transaction. */
-    tx.getInput(i, &from);
-    if(from == myFriendAddress){
-      /* Loop over my address and check the payment value. */
-      for(int j=0;j<tx.getOutputsCount();j++)
-      {
-        /* Create an empty address object. */
-        BitcoinAddress to;
-        /* Retreiving address from transaction. */
-        tx.getOutput(j, &to);
-        if(to == myAddress){
-          /* Check payment value and get amount using address index. */
-          if(tx.getOutputAmount(j) >= PAYMENT_VALUE){
-              /* Turn on the light bulb. */
-              digitalWrite(LIGHT_BULB, HIGH);
-              /* Delay for a second. */
-              Koyn.delay(1000);
-              /* Turn off the light bulb. */
-              digitalWrite(LIGHT_BULB, LOW); 
-          }
-        break;          
-        }
-      }
-      break;
+    /* Create an empty address. */
+    BitcoinAddress to;
+    /* Retreiving to address from transaction. */
+    tx.getOutput(i, &to);
+    /* Check if the bitcoins were sent to me. */
+    if (to == myAddress)
+    {
+      /* Sum up the amount. */
+      totalAmountSentToMe += tx.getOutputAmount(i);
     }
+  }
+
+  /* Check if the total amount sent to me is enough. */
+  if (totalAmountSentToMe >= PAYMENT_VALUE)
+  {
+    /* Turn on the light bulb. */
+    digitalWrite(LIGHT_BULB_RELAY, HIGH);
+    /* Delay for a second. */
+    Koyn.delay(1000);
+    /* Turn off the light bulb. */
+    digitalWrite(LIGHT_BULB_RELAY, LOW);
   }
 }
